1a4d74e7f793494590f58010f37e7c149ecc6502
matthewkuria <mathewkwachira@gmail.com>
2024-11-11 23:12:37 +0300

    initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,48 @@
+import { Text, View, StyleSheet } from 'react-native';
+import ImageViewer from "@/components/ImageViewer";
+import Button from '@/components/Button';
+
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme='primary' label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+   button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+   imageContainer: {
+    flex: 1,
+  },
+   image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+    footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
